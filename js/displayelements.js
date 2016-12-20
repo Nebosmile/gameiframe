cb94ab4d18a1dd3a821cee7e6ab7dccca5e0d1a5
Unknown <akulov_pavel88@mail.ru>
2016-12-20 18:09:58 +0200

    first version of iframe
    adaptiv

--- js/displayelements.js
+++ js/displayelements.js
@@ -8,24 +8,27 @@ window.onload =function(){
     var info =          document.getElementsByClassName('i')[0];
     var closemenu =     document.getElementsByClassName('x')[0];
     var hide=           document.getElementsByClassName('hide')[0];
-    var statistic=      document.getElementsByClassName('statistic')[0];
-    var forstatistic=   document.getElementsByClassName('forstatistic')[0];
-    var statictable=    document.getElementsByClassName('statictable')[0];
-    var select=         document.getElementsByClassName('select')[0];
     var artibuteStyle = document.getElementsByTagName("style")[0];
     var background=     document.getElementsByClassName('background')[0];
     var full =          document.getElementsByClassName('full')[0];
     var mainwrap =      document.getElementsByClassName('mainwrap')[0];
     var iframewrapper =document.getElementsByClassName('iframewrapper')[0];
     var gamecontent = document.getElementById('gamecontent');
-    console.log(gamecontent);
+    var iframe = document.getElementsByTagName('iframe')[0];
+    var fullout = document.getElementById('fullout');
+    var fulls = document.getElementById('fulls');
+    var forfullscreen =document.getElementById('forfullscreen');
     var history;
 
 
 
-    background.addEventListener("click",function(){
-        forstatistic.style.display="none";
-    });
+
+
+
+
+
+
+
 
 
     function launchFullScreen(element) {
@@ -37,9 +40,28 @@ window.onload =function(){
             element.webkitRequestFullScreen();
         }
     }
+    function cancelFullScreen(){
+        if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+        }
+    }
+
     full.addEventListener("click",function(){
-        launchFullScreen(gamecontent)
+        launchFullScreen(fulls);
+
+        fullout.style.display='inline-block';
     });
+    fullout.addEventListener("click",function(){
+        fullout.style.display='none';
+        cancelFullScreen();
+
+
+
+    })
 
     buttomFavorit.addEventListener("click",function(){
         if(!buttomFavorit.classList.contains('checked')){
@@ -61,7 +83,6 @@ window.onload =function(){
             gamecontent.classList.add("gamecontenopen");
         }
         else{
-
             rightmenu.classList.remove('rightmenuopen');
             closemenu.classList.remove('openx1');
             info.style.display="block";
@@ -73,6 +94,7 @@ window.onload =function(){
         closemenu.classList.add('openx1');
         info.style.display="none";
 
+
         }
         else{
             rightmenu.classList.remove('rightmenuopen');
@@ -80,92 +102,24 @@ window.onload =function(){
             info.style.display="block";
             iframewrapper.classList.remove("closeiframewrapper");
             gamecontent.classList.remove("gamecontenopen");
-        }
-    });
-
-
-
-    ////////////////////////////////////////Ajax
-
-    function getXmlHttp(){
-        var xmlhttp;
-        try{
-            xmlhttp = new ActiveXObject("Msxm12.XMLHTTP");
-        }   catch(e){
-            try{
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (E){
-                xmlhttp = false;
-            }
-
-        }
-        if(!xmlhttp && typeof XMLHttpRequest != "undefined" ){
-            xmlhttp = new XMLHttpRequest();
-            return xmlhttp;
-        }
-    }
-    function loadstatic(){
-        var xmlhttp = getXmlHttp();
-        xmlhttp.open("POST", "js/history.php",true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("");
-        xmlhttp.onreadystatechange = function(){
-            if(xmlhttp.readyState ==4){
-                if(xmlhttp.status ==200){
-
-                    history=JSON.parse(xmlhttp.responseText);
-                    history=history.history;
-                    makestatistic(history);
-                    forstatistic.style.display ="block";
-                }
-            }
-        }
-    }
-
-    statistic.addEventListener("click",function(){
-
-        loadstatic();
-    });
-
-    function makestatistic(element){
-        statictable.innerHTML="";
-        for(i=0;i<element.length;i++){
-            var tr = document.createElement('tr');
-            var td1 = document.createElement('td');
-            var td2 = document.createElement('td');
-            var td3 = document.createElement('td');
-            var date =new Date(element[i]["date"]);
-
-            td1.innerHTML=date.getDay()+"."+date.getMonth()+"."+date.getFullYear()+", "+ date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-            if(element[i]["operation"]=="bet"){
-                td2.innerHTML="Ставка";
-                tr.setAttribute("bet","");
-            }
-            else{
-                td2.innerHTML="Выиграш";
-                tr.setAttribute("win","");
-            }
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            td3.innerHTML=element[i]["sum"];
-            statictable.appendChild(tr);
-        }
-    }
-
-    select.addEventListener("change",function(){
-        if(select.value=="all"){
-            artibuteStyle.innerHTML=""
-        }
-        else if(select.value=="bet"){
-            artibuteStyle.innerHTML="tr[win]{display: none;} tr[bet]:nth-of-type(n){background-color: #091100;}"
-
-        }
-        else if(select.value=="win"){
-            artibuteStyle.innerHTML="tr[bet]{display: none;} tr[win]:nth-of-type(n){background-color: #091100;}"
 
         }
     });
+
+//////////////////////////////////////////keypress
+
+    window.onkeyup = pressed;
+    function pressed(e) {
+
+        if(e.which == 27){
+            fullout.style.display='none';
+            console.log("Нажата клавиша, ее значение ASCII: " + e.which);
+        }
+        else if(e.which ==122){
+            fullout.style.display='none';
+            console.log("Нажата клавиша, ее значение ASCII: " + e.which);
+        }
+    }
 
     closemenu.addEventListener("click",function(){
         mainwrap.remove()
